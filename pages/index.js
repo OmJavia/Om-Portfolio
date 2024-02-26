872aec5e481e426744a3a89fe2966b44bbd4591f
OmJavia <omjavia18@gmail.com>
2024-02-26 23:33:27 +0530

    New Resume

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
               />
               </li>
               <li>
-                <Link href="https://drive.google.com/file/d/1k3wRucUOt8tJa6lBhsS8msZ7dd_1IqlR/view?usp=sharing" target="_blank" className="font-bolder bg-gradient-to-r from-blue-700 to-blue-500 text-white font-bolder px-4 py-2 rounded-md ml-8 font-semibold shadow-md dar:shadow-slate-400">Resume</Link>
+                <Link href="https://drive.google.com/file/d/1rvRDgSp8flH74LsiUg1m_LpyWuxwWcXA/view?usp=sharing" target="_blank" className="font-bolder bg-gradient-to-r from-blue-700 to-blue-500 text-white font-bolder px-4 py-2 rounded-md ml-8 font-semibold shadow-md dar:shadow-slate-400">Resume</Link>
               </li>
             </ul>
           </nav>
