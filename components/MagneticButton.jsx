e8c34d5f6f0dc12380c9ce6868f2c9bce7ffea59
OmJavia <omjavia18@gmail.com>
2026-08-20 23:15:58 +0530

    Updated Theme of the website

--- components/MagneticButton.jsx
+++ components/MagneticButton.jsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import { motion, useMotionValue, useSpring } from 'framer-motion';
-
-export default function MagneticButton({ children, href, ...props }) {
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
-  const springX = useSpring(x, { stiffness: 220, damping: 18 });
-  const springY = useSpring(y, { stiffness: 220, damping: 18 });
-
-  const move = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    x.set((e.clientX - (rect.left + rect.width / 2)) * 0.18);
-    y.set((e.clientY - (rect.top + rect.height / 2)) * 0.18);
-  };
-  const reset = () => { x.set(0); y.set(0); };
-
-  return (
-    <motion.a
-      href={href}
-      style={{ x: springX, y: springY }}
-      onMouseMove={move}
-      onMouseLeave={reset}
-      whileTap={{ scale: 0.97 }}
-      {...props}
-    >
-      {children}
-    </motion.a>
-=======
 "use client";
 import { useRef } from "react";
 import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
@@ -120,6 +92,5 @@ export default function MagneticButton({
     >
       {children}
     </motion.button>
->>>>>>> b33423f (Updated Theme of the website)
   );
 }
